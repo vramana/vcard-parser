31beec6a4d72cfff1051466e2f521dcaa87cfbd2
Ramana Venkata <vramana@users.noreply.github.com>
2023-07-12 11:39:46 +0530

    Fixed the error

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,9 @@ use nom::{
     branch::alt,
     bytes::complete::{tag, tag_no_case, take_until, take_while_m_n},
     combinator::map_res,
-    error::context,
+    error::{context, convert_error},
     multi::{many_till, separated_list0, separated_list1},
-    sequence::{preceded, separated_pair, terminated, Tuple},
+    sequence::{preceded, separated_pair, terminated, tuple},
     IResult, Parser,
 };
 use std::error::Error;
@@ -40,12 +40,12 @@ struct VCard<'a> {
 }
 
 fn parse_vcf_begin(input: &str) -> IResult<&str, ()> {
-    let (input, _) = tag_no_case("BEGIN:VCARD\n")(input)?;
+    let (input, _) = tuple((tag_no_case("BEGIN:VCARD"), tag(LF)))(input)?;
     Ok((input, ()))
 }
 
 fn parse_vcf_end(input: &str) -> IResult<&str, ()> {
-    let (input, _) = tag_no_case("END:VCARD\n")(input)?;
+    let (input, _) = tuple((tag_no_case("END:VCARD"), tag(LF)))(input)?;
     Ok((input, ()))
 }
 
@@ -54,6 +54,7 @@ static COLON: &str = ":";
 static SEMI: &str = ";";
 static LF: &str = "\r\n";
 static COMMA: &str = ",";
+static END: &str = "END";
 
 fn parse_property_parameter(input: &str) -> IResult<&str, (&str, &str)> {
     separated_pair(
@@ -64,34 +65,48 @@ fn parse_property_parameter(input: &str) -> IResult<&str, (&str, &str)> {
 }
 
 fn parse_property_name(input: &str) -> IResult<&str, &str> {
-    alt((take_until(SEMI), take_until(COLON)))(input)
+    let (input, name) = alt((take_until(SEMI), take_until(COLON)))(input)?;
+
+    if name.to_uppercase() == END {
+        Err(nom::Err::Error(nom::error::Error::new(
+            input,
+            nom::error::ErrorKind::Tag,
+        )))
+    } else {
+        Ok((input, name))
+    }
+}
+
+fn parse_parameters(input: &str) -> IResult<&str, (Vec<(&str, &str)>, &str)> {
+    many_till(preceded(tag(SEMI), parse_property_parameter), tag(COLON))(input)
 }
 
 fn parse_property_value(input: &str) -> IResult<&str, Vec<&str>> {
-    separated_list0(tag(COMMA), alt((take_until(COMMA), take_until(LF))))(input)
+    let (input, v) = take_until(LF)(input)?;
+    Ok((input, vec![v]))
 }
 
 fn parse_property(input: &str) -> IResult<&str, Property> {
     let (input, name) = parse_property_name(input)?;
-    let (input, (params, _)) = context(
-        "params",
-        many_till(preceded(tag(SEMI), parse_property_parameter), tag(COLON)),
-    )(input)?;
+    let (input, (params, _)) = parse_parameters(input)?;
     let (input, value) = parse_property_value(input)?;
 
-    Ok((
-        input,
-        Property {
-            name,
-            params,
-            value,
-            group: None,
-        },
-    ))
+    let property = Property {
+        name,
+        params,
+        value,
+        group: None,
+    };
+
+    Ok((input, property))
+}
+
+fn parse_properties(input: &str) -> IResult<&str, Vec<Property>> {
+    separated_list1(tag(LF), parse_property)(input)
 }
 
 fn parse_version_3(input: &str) -> IResult<&str, usize> {
-    let (input, _) = tag_no_case("VERSION:3.0\n")(input)?;
+    let (input, _) = tuple((tag_no_case("VERSION:3.0"), tag(LF)))(input)?;
     Ok((input, 3))
 }
 
@@ -99,7 +114,8 @@ fn parse(input: &str) -> IResult<&str, Vec<Property>> {
     let (input, _) = parse_vcf_begin(input)?;
     let (input, _) = parse_version_3(input)?;
 
-    let (input, properties) = separated_list1(tag(LF), parse_property)(input)?;
+    let (input, properties) = parse_properties(input)?;
+    let (input, _) = tag(LF)(input)?;
 
     let (input, _) = parse_vcf_end(input)?;
 
@@ -127,27 +143,15 @@ fn unfold(input: &mut String) {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    println!("Hello, world!");
-
     let mut text_file = TEST_STRING.to_string();
 
     unfold(&mut text_file);
 
-    /*
-    error[E0515]: cannot return value referencing local variable `text_file`
-       --> src/main.rs:136:27
-        |
-    136 |     let (_, properties) = parse(&text_file)?;
-        |                           ^^^^^^----------^^
-        |                           |     |
-        |                           |     `text_file` is borrowed here
-        |                           returns a value referencing data owned by the current function
+    let (_, properties) = parse(&text_file).map_err(|e| e.to_owned())?;
 
-     */
-
-    let (_, properties) = parse(&text_file)?;
-
-    println!("{:?}", parse_property_name("check:test"));
+    for p in properties.iter() {
+        println!("{:?}", p);
+    }
 
     Ok(())
 }
@@ -162,6 +166,10 @@ mod tests {
             parse_property_parameter("hello=test;"),
             Ok(((";"), ("hello", "test"))),
         );
+        assert_eq!(
+            parse_parameters(";hello=test:"),
+            Ok(((";"), (vec![("hello", "test")], ""))),
+        );
     }
 
     #[test]
@@ -175,16 +183,22 @@ mod tests {
         assert_eq!(parse_property_value("test\r\n"), Ok(("\r\n", vec!["test"])),);
         assert_eq!(
             parse_property_value("hello,test\r\n"),
-            Ok(("\r\n", vec!["hello", "test"])),
+            Ok(("\r\n", vec!["hello,test"])),
         );
         assert_eq!(
             parse_property_value("al  hello,test\r\n"),
-            Ok(("\r\n", vec!["al  hello", "test"])),
+            Ok(("\r\n", vec!["al  hello,test"])),
         );
     }
 
     #[test]
     fn property() {
+        let test = "FN:Hello Betty\r\nN:Hello;Betty\r\n";
+        assert_eq!(
+            // parse_properties("FN:Hello Betty\r\nN:Hello;Betty;;;\r\n"),
+            parse_properties(test),
+            Ok(("\r\n", vec![]))
+        );
         assert_eq!(
             parse_property("fn:test\r\n"),
             Ok((
@@ -205,24 +219,37 @@ mod tests {
                     group: None,
                     name: "fn",
                     params: vec![("type", "internet")],
-                    value: vec!["test", "time"]
+                    value: vec!["test,time"]
                 }
+            )),
+        );
+
+        assert_eq!(
+            parse_properties("fn:test\r\nEND:VCARD\r\n"),
+            Ok((
+                "\r\nEND:VCARD\r\n",
+                vec![Property {
+                    group: None,
+                    name: "fn",
+                    params: vec![],
+                    value: vec!["test"]
+                }]
             )),
         );
     }
 }
 
-static TEST_STRING: &str = "BEGIN:VCARD
-VERSION:3.0
-FN:Hello Betty
-N:Hello;Betty;;;
-EMAIL;TYPE=INTERNET:hello.betty@gmail.com
-TEL;TYPE=CELL:+91 12342 12332
-TEL;TYPE=CELL:+1 (123) 112-123
-ROLE:Application Engineer
-NOTE:Gender: Male
-PHOTO:https://lh3.googleusercontent.com/contacts/AOq4LdZ2EOkQkPc_KK2CyLAkx1
- 8rcOgp0FYDG3f9_omOYadasd
-CATEGORIES:myContacts
-END:VCARD
+static TEST_STRING: &str = "BEGIN:VCARD\r
+VERSION:3.0\r
+FN:Hello Betty\r
+N:Hello;Betty;;;\r
+EMAIL;TYPE=INTERNET:hello.betty@gmail.com\r
+TEL;TYPE=CELL:+91 12342 12332\r
+TEL;TYPE=CELL:+1 (123) 112-123\r
+ROLE:Application Engineer\r
+NOTE:Gender: Male\r
+PHOTO:https://lh3.googleusercontent.com/contacts/AOq4LdZ2EOkQkPc_KK2CyLAkx1\r
+ 8rcOgp0FYDG3f9_omOYadasd\r
+CATEGORIES:myContacts\r
+END:VCARD\r
 ";
